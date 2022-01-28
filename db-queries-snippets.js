0dac31d16a59a1c421fefed421676b04bfe6a2e5
Dmitriy Kharlanov <kharlanov-d@yandex.com>
2022-01-28 19:13:14 +0300

    a searchResults query

--- db-queries-snippets.js
+++ db-queries-snippets.js
@@ -5,3 +5,11 @@ db.getCollection('priceCoverages')
         { $sort : { _id : 1 } },
     ] ).toArray().map(ibe => `${ibe._id} - ${ibe.avgScore}`)
 
+db.getCollection('searchResults').find(
+    {
+        'search.hotelId': "12633531324025229998H2337470068379471370",
+        'search.nights': { $lte: 7 },
+        'checkInAt': { $gte: new Date(), $lte: new Date(new Date().setDate(new Date().getDate() + 90)) },
+        'result.crawledAt': { $gt: new Date('2022-01-14T00:00:00').getTime() },
+    }
+).count()
