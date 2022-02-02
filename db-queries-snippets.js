26fb92426761c9be409f5aa782abaeebb547c827
Dmitriy Kharlanov <kharlanov-d@yandex.com>
2022-02-02 12:14:54 +0300

    more queries for price coverage

--- db-queries-snippets.js
+++ db-queries-snippets.js
@@ -5,11 +5,61 @@ db.getCollection('priceCoverages')
         { $sort : { _id : 1 } },
     ] ).toArray().map(ibe => `${ibe._id} - ${ibe.avgScore}`)
 
+// all results
 db.getCollection('searchResults').find(
     {
-        'search.hotelId': "12633531324025229998H2337470068379471370",
+        'search.hotelId': "4404277886483379677H14819020022498232905",
         'search.nights': { $lte: 7 },
         'checkInAt': { $gte: new Date(), $lte: new Date(new Date().setDate(new Date().getDate() + 90)) },
-        'result.crawledAt': { $gt: new Date('2022-01-14T00:00:00').getTime() },
+        'result.crawledAt': { $gt: new Date('2022-01-17T00:00:00').getTime() },
+    }
+).count()
+
+// crawledAt <= 6h
+db.getCollection('searchResults').find(
+    {
+        'search.hotelId': "14810592085688282439H10051094366830057032",
+        'search.nights': { $lte: 7 },
+        'checkInAt': { $gte: new Date(), $lte: new Date(new Date().setDate(new Date().getDate() + 3)) },
+        'result.crawledAt': { $gte: new Date('2022-01-30T17:35:00').getTime(), $lt: new Date('2022-01-31T11:35:00').getTime() },
+    }
+).count()
+
+// const ms = require('./ms.js');
+function fromNow(interval) {
+    return new Date(Date.now() + ms(interval));
+}
+function getCheckInAt(interval) {
+    const checkIn = new Date(fromNow(interval));
+    checkIn.setHours(0, 0, 0, 0);
+
+    return checkIn;
+}
+
+db.getCollection('searchResults').find(
+    {
+        'search.hotelId': "4404277886483379677H14819020022498232905",
+        'search.nights': { $lte: 7 },
+        $or: [
+            {
+                'result.crawledAt': { $gt: fromNow('-6h').getTime() },
+            },
+            {
+                'checkInAt': { $gt: getCheckInAt('1d'), $lte: getCheckInAt('3d') },
+                'result.crawledAt': { $gte: fromNow('-1d').getTime(), $lt: fromNow('-6h').getTime() },
+            },
+            {
+                'checkInAt': { $gt: getCheckInAt('3d'), $lte: getCheckInAt('7d') },
+                'result.crawledAt': { $gte: fromNow('-3d').getTime(), $lt: fromNow('-1d').getTime() },
+            },
+            {
+                'checkInAt': { $gt: getCheckInAt('7d'), $lte: getCheckInAt('30d') },
+                'result.crawledAt': { $gte: fromNow('-7d').getTime(), $lt: fromNow('-3d').getTime() },
+            },
+            {
+                'checkInAt': { $gt: getCheckInAt('30d'), $lte: getCheckInAt('90d') },
+                'result.crawledAt': { $gte: fromNow('-14d').getTime(), $lt: fromNow('-7d').getTime() },
+            },
+        ],
     }
 ).count()
---
+++ search-results-for-price-coverage.js
@@ -0,0 +1,197 @@
+
+var s = 1000;
+var m = s * 60;
+var h = m * 60;
+var d = h * 24;
+var w = d * 7;
+var y = d * 365.25;
+
+
+function ms (val, options) {
+    options = options || {};
+    var type = typeof val;
+    if (type === 'string' && val.length > 0) {
+        return parse(val);
+    } else if (type === 'number' && isFinite(val)) {
+        return options.long ? fmtLong(val) : fmtShort(val);
+    }
+    throw new Error(
+        'val is not a non-empty string or a valid number. val=' +
+        JSON.stringify(val)
+    );
+};
+
+function parse(str) {
+    str = String(str);
+    if (str.length > 100) {
+        return;
+    }
+    var match = /^(-?(?:\d+)?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$/i.exec(
+        str
+    );
+    if (!match) {
+        return;
+    }
+    var n = parseFloat(match[1]);
+    var type = (match[2] || 'ms').toLowerCase();
+    switch (type) {
+        case 'years':
+        case 'year':
+        case 'yrs':
+        case 'yr':
+        case 'y':
+            return n * y;
+        case 'weeks':
+        case 'week':
+        case 'w':
+            return n * w;
+        case 'days':
+        case 'day':
+        case 'd':
+            return n * d;
+        case 'hours':
+        case 'hour':
+        case 'hrs':
+        case 'hr':
+        case 'h':
+            return n * h;
+        case 'minutes':
+        case 'minute':
+        case 'mins':
+        case 'min':
+        case 'm':
+            return n * m;
+        case 'seconds':
+        case 'second':
+        case 'secs':
+        case 'sec':
+        case 's':
+            return n * s;
+        case 'milliseconds':
+        case 'millisecond':
+        case 'msecs':
+        case 'msec':
+        case 'ms':
+            return n;
+        default:
+            return undefined;
+    }
+}
+
+function fmtShort(ms) {
+    var msAbs = Math.abs(ms);
+    if (msAbs >= d) {
+        return Math.round(ms / d) + 'd';
+    }
+    if (msAbs >= h) {
+        return Math.round(ms / h) + 'h';
+    }
+    if (msAbs >= m) {
+        return Math.round(ms / m) + 'm';
+    }
+    if (msAbs >= s) {
+        return Math.round(ms / s) + 's';
+    }
+    return ms + 'ms';
+}
+
+function fmtLong(ms) {
+    var msAbs = Math.abs(ms);
+    if (msAbs >= d) {
+        return plural(ms, msAbs, d, 'day');
+    }
+    if (msAbs >= h) {
+        return plural(ms, msAbs, h, 'hour');
+    }
+    if (msAbs >= m) {
+        return plural(ms, msAbs, m, 'minute');
+    }
+    if (msAbs >= s) {
+        return plural(ms, msAbs, s, 'second');
+    }
+    return ms + ' ms';
+}
+
+function plural(ms, msAbs, n, name) {
+    var isPlural = msAbs >= n * 1.5;
+    return Math.round(ms / n) + ' ' + name + (isPlural ? 's' : '');
+}
+
+
+
+
+
+
+
+
+
+var hotelId = "18145247091935012804H3922879677499527945";
+
+function fromNow(interval) {
+    return new Date(Date.now() + ms(interval)).getTime();
+}
+function getCheckInAt(interval) {
+    const checkIn = new Date(fromNow(interval));
+    checkIn.setHours(0, 0, 0, 0);
+
+    return checkIn;
+}
+
+db.getCollection('searchResults').find(
+    {
+        'search.hotelId': hotelId,
+        'search.nights': { $lte: 7 },
+        'checkInAt': { $gte: getCheckInAt('1d'), $lte: getCheckInAt('90d') },
+        'result.crawledAt': { $gte: fromNow('-14d') },
+    }
+).count()
+
+db.getCollection('searchResults').find(
+    {
+        'search.hotelId': hotelId,
+        'search.nights': { $lte: 7 },
+        $or: [
+            {
+                'result.crawledAt': { $gt: fromNow('-6h') },
+            },
+            {
+                'checkInAt': { $gte: getCheckInAt('3d'), $lte: getCheckInAt('90d') },
+                'result.crawledAt': { $gte: fromNow('-1d'), $lt: fromNow('-6h') },
+            },
+            {
+                'checkInAt': { $gte: getCheckInAt('7d'), $lte: getCheckInAt('90d') },
+                'result.crawledAt': { $gte: fromNow('-3d'), $lt: fromNow('-1d') },
+            },
+            {
+                'checkInAt': { $gte: getCheckInAt('30d'), $lte: getCheckInAt('90d') },
+                'result.crawledAt': { $gte: fromNow('-7d'), $lt: fromNow('-3d') },
+            },
+        ],
+    }
+).count()
+
+db.getCollection('searchResults').find(
+    {
+        'search.hotelId': hotelId,
+        'search.nights': { $lte: 7 },
+        $or: [
+            {
+                'result.crawledAt': { $gt: fromNow('-6h') },
+            },
+            {
+                'checkInAt': { $gte: getCheckInAt('3d'), $lte: getCheckInAt('90d') },
+                'result.crawledAt': { $gte: fromNow('-1d'), $lt: fromNow('-6h') },
+            },
+            {
+                'checkInAt': { $gte: getCheckInAt('7d'), $lte: getCheckInAt('90d') },
+                'result.crawledAt': { $gte: fromNow('-3d'), $lt: fromNow('-1d') },
+            },
+            {
+                'checkInAt': { $gte: getCheckInAt('30d'), $lte: getCheckInAt('90d') },
+                'result.crawledAt': { $gte: fromNow('-7d'), $lt: fromNow('-3d') },
+            },
+        ],
+    }
+).count()/630
+
+db.getCollection('priceCoverages').find({hotelId: hotelId})
